be566b49d75ede00313d87eb538b16aef440daeb
javabydevdutt <javabydevdutt@gmail.com>
2022-05-24 14:57:10 +0530

    adding api and contact information in swagger configuration

--- src/main/java/com/devdutt/swagger/config/SwaggerConfiguration.java
+++ src/main/java/com/devdutt/swagger/config/SwaggerConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +19,26 @@ public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	public Docket createDocket() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.devdutt.swagger")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.devdutt.swagger"))
+				.build()
+				.apiInfo(apiInfo());
 	}
 
+	public ApiInfo apiInfo() {
+		return new ApiInfo(
+					"Spring Boot REST API", 
+					"Spring Boot REST API for Demo Application", 
+					"1.0",
+					"Terms of service", 
+					 new Contact(
+							 "Devdutt", 
+							 "http://www.infosys.com/", 
+							 "devduttjatav@gmail.com"
+					),
+					"Apache License Version 2.0",
+					"https://www.apache.org/licenses/LICENSE-2.0"
+				);
+	}
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// enabling swagger-ui part for visual documentation
